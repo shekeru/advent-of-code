023126b89c435610ab995fcdcf231819634587e0
Sheky Sheks <sheks@desu.systems>
2018-12-02 15:36:29 -0500

    day 2 haskell

--- 2018/Day 2/Solution.hs
+++ 2018/Day 2/Solution.hs
@@ -1,6 +1,30 @@
 module Solution where
 
-main = print ""
+import Control.Monad
+import Text.Printf
+import Data.List
+
+main :: IO()
+main = mapM_ (input >>=) [
+  printf "part 1: %d\n".checkSum,
+  printf "part 2: %s\n".head.concatMap
+    match.iterate tail]
 
 input :: IO [String]
 input = lines<$>readFile "input.txt"
+
+count :: Eq a => [a] -> [a] -> [Int]
+count elems list = map ($list) $map (\y ->
+  length.filter (y ==)) elems
+
+checkSum :: Foldable t => t String -> Int
+checkSum = product.count [2,3].concatMap (intersect [2,3].count ['a'..'z'])
+
+match :: Eq t => [[t]] -> [[t]]
+match (current:list) = do
+    option <- charXor current<$>list
+    let check = length option + 1 == length current
+    guard check; return option
+
+charXor :: Eq t => [t] -> [t] -> [t]
+charXor xs ys = [x | (x,y) <- zip xs ys, x == y]
