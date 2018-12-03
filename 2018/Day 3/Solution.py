c1d13055c7c03735c60467deb47af014398c013c
Sheky Sheks <sheks@desu.systems>
2018-12-03 01:47:39 -0500

    day 3. part 2, python only

--- 2018/Day 3/Solution.py
+++ 2018/Day 3/Solution.py
@@ -1,19 +1,20 @@
 import re, collections
 with open('input.txt') as f:
-    parse = lambda x: map(int, re.findall(r'\d+', x))
-    xs = map(parse, f.read().splitlines())
+    parse = lambda x: [*map(int, re.findall(r'\d+', x))]
+    xs = [*map(parse, f.read().splitlines())]
 table = collections.defaultdict(int)
-def apply(_, x ,y ,w, h):
+def apply(_, *vals):
+    for k in coords(*vals):
+        table[k] += 1
+def coords(x, y, w, h):
     for i in range(y, y+h):
         for j in range(x, x+w):
-            table[(i,j)] +=1
-def match(lines):
+            yield (i, j)
+def search(lines):
     for (number, x, y, w, h) in lines:
-        if sum(table[(i,j)] for i in range(y, y+h)
-            for j in range(x, x+w)) == w*h:
-                return number
-[apply(*line) for line in xs]
-overlap = sum([1 for v in table.values() if v > 1])
-print('part 1:', overlap)
-#print('part 2:', match(xs))
-input()
+        if w * h == sum(table[k] for k in
+            coords(x, y, w, h)): return number
+_, overlap = [apply(*line) for line in xs], sum(
+    [1 for v in table.values() if v > 1]
+); print('part 1:', overlap)
+print('part 2:', search(xs))
