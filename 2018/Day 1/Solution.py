7af06776d3c83709f8344ecb91d4ba2fe9637e49
Sheky Sheks <sheks@desu.systems>
2018-12-02 22:23:08 -0500

    fuck c

--- 2018/Day 1/Solution.py
+++ 2018/Day 1/Solution.py
@@ -1,8 +1,10 @@
+import itertools
 with open('input.txt') as f:
     xs = [*map(int, f.read().splitlines())]
 def cycle(xs):
     seen, current = {0}, 0
     while True:
+        print(1)
         for x in [*xs]:
             current += x
             if current in seen:
@@ -11,4 +13,3 @@ def cycle(xs):
                 seen.add(current)
 print('part 1:', sum(xs))
 print('part 2:', cycle(xs))
-input()
