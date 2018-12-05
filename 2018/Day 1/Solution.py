3a50a7753780743e127ef01581153f167e0adda4
Sheky Sheks <sheks@desu.systems>
2018-12-05 13:35:17 -0500

    remove wish code

--- 2018/Day 1/Solution.py
+++ 2018/Day 1/Solution.py
@@ -11,26 +11,3 @@ def cycle(xs):
                 seen.add(current)
 print('part 1:', sum(xs))
 print('part 2:', cycle(xs))
-
-frequency = 0
-with open("input.txt", "r") as fp:
-    for line in fp:
-        if line[0] == "+":
-            frequency += int(line[1:-1])
-        else:
-            frequency -= int(line[1:-1])
-print(f"Final frequency: {frequency}")
-
-frequencies = open("input.txt", "r").read().strip().split("\n")
-total_frequency = 0
-previous_freqs = {0}
-while True:
-    for frequency in frequencies:
-        if frequency.startswith("+"):
-            total_frequency += int(frequency[1:])
-        else:
-            total_frequency -= int(frequency[1:])
-        if total_frequency in previous_freqs:
-            print(f"Duplicate frequency: {total_frequency}")
-            exit()
-        previous_freqs.add(total_frequency)
