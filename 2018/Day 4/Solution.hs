12cebf21c41d6011a56db3a3bec04be9850a1c66
Sheky Sheks <sheks@desu.systems>
2018-12-05 13:34:34 -0500

    no seq needed lol

--- 2018/Day 4/Solution.hs
+++ 2018/Day 4/Solution.hs
@@ -6,6 +6,7 @@ import Text.Parsec hiding (State)
 import Text.Parsec.String
 import Control.Monad.State
 import Data.List.Split
+import Data.Function
 import Control.Monad
 import Text.Printf
 import Data.List
@@ -16,29 +17,35 @@ type Matrix = Map.IntMap (Map.IntMap Int)
 type Active = (Map.Key, Int, Matrix)
 
 main :: IO()
-main = do
-  list <- sortOn stamp<$>input
-  let result = (`evalState` start) $ trackTimes list
-  print result where start = (0, 0, Map.empty)
+main = (sortOn stamp<$>input) >>= \y ->
+  mapM_ ($trackTimes y `evalState` start) [
+    printf "part 1: %d\n".solve sum,
+    printf "part 2: %d\n".solve maximum
+    ] where start = (0, 0, Map.empty)
 
 input :: IO [Entry]
 input = readFile "input.txt" >>= \y->
   pure$ case parse (many parseEvent) "" y of
     Left err -> []; Right xs -> xs
 
+solve :: (Map.IntMap Int -> Int) -> Matrix -> Int
+solve func kxvs = key * value where
+  greatest = maximumBy (compare `on` snd).Map.assocs
+  (key, _) =  greatest $Map.map func kxvs
+  (value, _) = greatest (kxvs Map.! key)
+
 trackTimes :: [Entry] -> State Active Matrix
 trackTimes [] = get >>= yield
   where yield (_, _, r) = pure r
-trackTimes (action:future) = do
+trackTimes (action:remaining) = do
   (key, start, current) <- get
   let active = Map.findWithDefault Map.empty key current
   let new_state = update active [start..time action-1]
-  case event action of
-    Guard new -> put (new, start, current)
-    Sleeps -> put (key, time action, current)
-    Wakes -> put (key, start, Map.insert key
-      new_state current)
-  trackTimes future where
+  put $ case event action of
+    Wakes -> (key, start, Map.insert key new_state current)
+    Sleeps -> (key, time action, current)
+    Guard new -> (new, start, current)
+  trackTimes remaining where
     update = foldl (\dict x -> Map.insertWith (+) x 1 dict)
 
 parseEvent :: Parser Entry
@@ -47,8 +54,7 @@ parseEvent = do
   let seconds = (read.last) $ splitOn ":" initial
   action <- spaces *> many letter
   value <- try (string " #" *> many1 digit)
-    <|> manyTill anyChar newline
-  try (manyTill anyChar newline)
+    <|> many space; manyTill anyChar endOfLine
   return$ Entry initial seconds $ case action of
     "falls" -> Sleeps; "wakes" -> Wakes
     "Guard" -> Guard $read value
