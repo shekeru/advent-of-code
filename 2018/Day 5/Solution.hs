edeb29e9b13f91d0c9f25d5c6aaaa86c7fd1d593
Sheky Sheks <sheks@desu.systems>
2018-12-05 16:22:13 -0500

    there has to be a cleaner way

---
+++ 2018/Day 5/Solution.hs
@@ -0,0 +1,25 @@
+{-#LANGUAGE PartialTypeSignatures#-}
+module Solution where
+
+import Data.Function
+import Text.Printf
+import Data.Char
+
+main :: IO()
+main =
+  mapM_ (input >>=) [
+   printf "part 1: %d\n".solve,
+   printf "part 2: %d\n".minimum.map solve.swaps]
+    where solve = length.foldr reduction ""
+
+input :: IO String
+input = init<$>readFile "input.txt"
+
+reduction :: Char -> String -> String
+reduction x [] = [x]
+reduction x zs@(y:ys) = if reacts x y then ys else x:zs
+  where reacts x y = x /= y && on (==) toLower x y
+
+swaps :: String -> [String]
+swaps xs = map (($xs).strip) ['a'..'z']
+  where strip x = filter (not.flip elem [x, toUpper x])
