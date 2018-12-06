637ed75aa376edc5d0c3e99074edd873f86e24db
Sheky Sheks <sheks@desu.systems>
2018-12-06 05:48:57 -0500

    it runs lol

--- 2018/Day 6/Solution.hs
+++ 2018/Day 6/Solution.hs
@@ -9,37 +9,22 @@ import Data.List
 
 type Coords = (Int, Int)
 
---main :: IO()
-main = print ""
+main :: IO()
+main = mapM_ (input >>=) [
+   printf "part 1: %d\n".enclosed,
+   printf "part 2: %d\n".adjacent]
 
 input :: IO [Coords]
 input = map (pair.parse).lines<$>readFile "input.txt"
   where pair [a,b] = (a,b); parse = map read.splitOn ","
 
+enclosed :: [Coords] -> Int
 enclosed points = maximum.assemble'$ do
   ys <- range snd points; xs <- range fst points
   let table = distance' (xs, ys) <$> points
-  let opt = minimumBy (on compare fst) table
-  guard (length (filter (on (==) fst opt) table) == 1)
-  return $ snd opt
-
-assemble :: Eq a => [a] -> [(a, Int)]
-assemble xs = count xs <$> xs where
-  count xs x = (x, length $filter (== x) xs)
-
-assemble' :: Eq a => [a] -> [Int]
-assemble' xs = count xs <$> xs where
-  count xs x = length $filter (== x) xs
-
-test :: [Coords]
-test = [
-  (1, 1),
-  (1, 6),
-  (8, 3),
-  (3, 4),
-  (5, 5),
-  (8, 9)]
-
+  let (val, key) = minimumBy (on compare fst) table
+  guard (count (map fst table) val == 1)
+  return key
 
 adjacent :: [Coords] -> Int
 adjacent points = length [1 |
@@ -50,8 +35,14 @@ distance :: Coords -> Coords -> Int
 distance (x1,y1) (x,y) = on (+) abs (x1-x) (y1-y)
 
 distance' :: Coords -> Coords -> (Int, Coords)
-distance' (x1,y1) l@(x,y) = (on (+) abs (x1-x) (y1-y), l)
+distance' a b = (distance a b, b)
+
+assemble' :: Eq a => [a] -> [Int]
+assemble' xs = count xs <$> nub xs
 
 range :: (Coords -> Int) -> [Coords] -> [Int]
 range f xs = [minimum ys..maximum ys-1]
   where ys = map f xs
+
+count :: Eq a => [a] -> a -> Int
+count xs x = length $filter (== x) xs
