f0a7ab4f8a3c7151df1273305290896e0fac8e67
Sheky Sheks <sheks@desu.systems>
2018-12-07 02:36:16 -0500

    cleanup

--- 2018/Day 7/Solution.py
+++ 2018/Day 7/Solution.py
@@ -1,17 +1,16 @@
-import re, string, time
+import string, re
 with open('input.txt') as f:
     parse = lambda x: tuple(re.findall(r'[A-Z]', x)[1:])
     routes = [*map(parse, f.read().splitlines())]
 def value_seconds(letter, offset):
     return string.ascii_uppercase.find(letter) + offset
 def double_map(routes):
-    return {chr for line in routes for chr in line}
+    return {chr: set() for line in routes for chr in line}
 def forwards(routes, workers = 2, offset = 0):
-    table = {k: set() for k in double_map(routes)}
+    accum, queue, table = "", dict(), double_map(routes)
     for (req, key) in routes:
         table[key].add(req)
-    accum, queue = "", dict()
-    for seconds in range(1000000):
+    for seconds in range(1000**1000):
         queue = {k: v-1 for k,v in queue.items()}
         for worker in queue.copy():
             if queue[worker] < 1:
@@ -19,11 +18,9 @@ def forwards(routes, workers = 2, offset = 0):
                     table[rest].discard(worker)
                 accum += worker; del queue[worker]
         nexts = [*sorted(k for k,v in table.items() if not v)]
-        for i in range(min(len(nexts), workers)):
-            if len(queue) < workers:
-                queue[nexts[i]] = value_seconds(nexts[i],
-                    offset+1) if isinstance(offset, int) else 0
-                del table[nexts[i]]
+        for i in range(min(len(nexts), workers - len(queue))):
+            queue[nexts[i]] = (value_seconds(nexts[i],offset+1) if
+                isinstance(offset, int) else 0); del table[nexts[i]]
         if not len(table)+len(queue):
             return (seconds, accum)
 print('part 1:', forwards(routes, workers = 1, offset = None)[1])
