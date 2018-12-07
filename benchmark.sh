e27cecfcd921436f6bd2a3bd7b51ebdfbc5eb970
Sheky Sheks <sheks@desu.systems>
2018-12-07 02:23:49 -0500

    day 7 python

--- benchmark.sh
+++ benchmark.sh
@@ -3,7 +3,7 @@
 base=$(pwd)
 for year in */; do
   for day in $year*/; do
-    cd "$base/$day" && ghc "Solution.hs" -O3
+    cd "$base/$day" && ghc "Solution.hs" -O2
     echo $day && time "./Solution.exe" > /dev/null 2>&1;
     time python3 Solution.py > /dev/null 2>&1;
   done
