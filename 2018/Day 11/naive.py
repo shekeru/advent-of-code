5b6df0434406feb3bb212efc22861138bd8cda44
Sheky Sheks <sheks@desu.systems>
2018-12-11 03:35:21 -0500

    nice

--- 2018/Day 11/naive.py
+++ 2018/Day 11/naive.py
@@ -11,7 +11,7 @@ def find_max(pts, i = 3):
         double_range(1, 301 - i)}.items(), key = lambda xs: xs[1])
 pts = {pair: evaluate(8141, *pair) for pair in double_range(1, 301)}
 last_max, _ = 0, print('Silver: %s with %d' % find_max(pts, 3))
-for j in range(1, 1+301**0.5):
+for j in range(1, int(301**0.5)+1):
     coords, val = find_max(pts, j)
     if val > last_max:
         print('Option:',coords,'with', val)
