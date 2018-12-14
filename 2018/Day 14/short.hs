da6dd7f324bcbc682d2da65ba77090882258340a
Sheky Sheks <sheks@desu.systems>
2018-12-14 04:55:53 -0500

    fixes?

--- 2018/Day 14/short.hs
+++ 2018/Day 14/short.hs
@@ -7,18 +7,15 @@ type State = ([Int], Int, Int)
 main = do
   let const = 30121
   let (numbers, u, v) = sets!!(const+10)
-  --print$ take 20 $drop (const-10) $reverse numbers
-  mapM_ print (reverse numbers)
+  print$ take 20 $drop (const-10) $reverse numbers
+  --mapM_ print (reverse numbers)
 
 sets :: [State]
 sets = iterate next ([7,3], 0, 1)
 
 next :: State -> State
 next (xs, u, v) = (xs', f' u, f' v) where
-  f' j = g' $mod 1 + xs!!j
-  g' j = if j < 0 then length xs' + j else j
-    -- First Shift right by ys added
-    -- Then shift left by value + 1
-    -- if negative, then we have to loop
   xs' = foldl (flip (:)) xs$ if null ys then [0] else ys
-  ys = digits 10 (xs!!u + xs!!v)
+  f' j = mod (j + 1 + ret j) $length xs'
+  ret i = xs !! (length xs - 1 - i)
+  ys = digits 10 (ret u + ret v)
