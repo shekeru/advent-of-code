c4534c9e02204d2249fcadee7eefbd451ad95cbd
Sheky Sheks <sheks@desu.systems>
2019-06-21 20:21:10 -0400

    working finally

--- 2018/Day 15/goblins.py
+++ 2018/Day 15/goblins.py
@@ -13,19 +13,12 @@ class Unit:
     def __init__(self, elf, pt, dmg, hp = 200):
         self.elf, self.pt, self.hp = elf, pt, hp
         self.dmg = 3 if not elf else dmg
-    def alive(self):
-        if not self.hp:
-            if self.dmg > 3:
-                raise Exception('elf_died')
-            return False
-        return True
     
 class World(dict):
     def __init__(self, name, elf_dmg = 3):
         # WorldSpace Dict, and Entity List
+        super(); self.elf_dmg = elf_dmg
         self.units, self.name = [], name
-        self.dmg = elf_dmg; super()
-        # Read from file
         with open(name) as f:
             lines = [*map(list, f.read().splitlines())]
             for y, line in enumerate(lines):
@@ -41,60 +34,58 @@ class World(dict):
         slots = {pt for v in targets for pt in v.pt.nb4 if self.valid(pt)}
         while spaces:
             current = spaces.popleft()
-            if current in slots:
-                yield from (paths[current] or [current])
             for future in current.nb4:
                 if future not in paths and self.valid(future):
                     paths[future] = paths[current] + [future]
-                    spaces.append(future)
+                    if not {*paths} & slots: # do not break
+                        spaces.append(future)
+        opts = sorted({key for key in paths if key in slots},
+            key = lambda pt: (len(paths[pt]), pt)) + [unit.pt]
+        paths[unit.pt] = [unit.pt]; yield from paths[opts[0]]
     def valid(self, point):
         return self[point] and point not in self.active
     # Main Logic by Unit
     def on_turn(self, unit):
-        # Update all others/enemies
-        targets = [v for v in self.units if v.elf != unit.elf and v.alive()]
-        # None to Fight
+        targets = [v for v in self.units if v.elf != unit.elf and v.hp]
+        self.active = {v.pt for v in self.units if v.hp and v != unit}
+        # No enemies
         if not targets:
-            return True
-        # Select New Position
-        self.active = {v.pt for v in self.units if v.alive() and v != unit}
-        for crds in self.next_move(unit, targets):
-            unit.pt = crds; break
-        # Select Favored Target
+            return unit.dmg == self.elf_dmg
+        # Move, Find Nearby, Attack, Clamp HP
+        unit.pt = next(self.next_move(unit, targets))
         opponents = [v for v in targets if v.pt in unit.pt.nb4]
         if opponents:
             target = min(opponents, key = lambda v: (v.hp, v.pt))
             target.hp = max(0, target.hp - unit.dmg)
-    # Run All Unit Turns
-    def fight_all(self):
+    # Simulate battle
+    def fight_all(self, turns = 0):
         for unit in sorted(self.units, key = lambda x: x.pt):
-            if unit.alive():
+            if unit.hp:
                 if self.on_turn(unit):
-                    return True
+                    return turns
+            elif unit.elf and unit.dmg > 3:
+                return -turns
+        return self.fight_all(turns + 1)
     # Multipart score
     def get_score(self):
-        try:
-            i = 0
-            while not self.fight_all():
-                i += 1
-            #print(self.dmg, i, sum(map(lambda v: v.hp, self.units)))
-            return i * sum(map(lambda v: v.hp, self.units))
-        except Exception as eff:
-            print(self.dmg, i, sum(map(lambda v: v.hp, self.units)))
-            if 'elf_died' in eff.args:
-                return World(self.name, self.dmg + 1).get_score()
+        turns = self.fight_all()
+        if turns > 0:
+            return turns * sum(v.hp for v in self.units)
+        return World(self.name, self.elf_dmg + 1).get_score()
     
-print("Running part 1 tests...")
-#assert(27730 == World("test0.txt").get_score())
-#assert(36334 == World("test1.txt").get_score())
-#assert(39514 == World("test2.txt").get_score())
-#assert(27755 == World("test3.txt").get_score())
-#assert(28944 == World("test4.txt").get_score())
+print("Running section 1 tests...")
+assert(27730 == World("test0.txt").get_score())
+assert(36334 == World("test1.txt").get_score())
+assert(39514 == World("test2.txt").get_score())
+assert(27755 == World("test3.txt").get_score())
+assert(28944 == World("test4.txt").get_score())
 assert(18740 == World("test5.txt").get_score())
-#print("Silver: %d" % World("input.txt").get_score())
-#assert(4988 == World("test0.txt", 4).get_score())
-#assert(31284 == World("test2.txt", 4).get_score())
-#assert(3478 == World("test3.txt", 4).get_score())
-#assert(6474 == World("test4.txt", 4).get_score())
+print("Running section 2 tests...")
+assert(4988 == World("test0.txt", 4).get_score())
+assert(31284 == World("test2.txt", 4).get_score())
+assert(3478 == World("test3.txt", 4).get_score())
+assert(6474 == World("test4.txt", 4).get_score())
 assert(1140 == World("test5.txt", 4).get_score())
-print("Gold: %d" % World("input.txt", 15).get_score())
+
+print("Silver: %d" % World("input.txt").get_score())
+print("Gold: %d" % World("input.txt", 4).get_score())
