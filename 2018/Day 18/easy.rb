ac4e0459e96338be537b8d074a31afa75680e04b
Sheky Sheks <sheks@desu.systems>
2019-07-20 20:35:33 -0400

    fugg

--- 2018/Day 18/easy.rb
+++ 2018/Day 18/easy.rb
@@ -15,13 +15,15 @@ class Tile
     # Search Moore neighboorhood
     check = ->(type) do
       [*(-1..1)].product([*(-1..1)]).reduce(0) do |s, (y, x)|
-        s + if y+x && world[@y+y, @x+x]&.state == type
-    then 1 else 0 end end end
+        s + if world[@y+y, @x+x]&.state == type and \
+    [y, x] != [0, 0] then 1 else 0 end end end
     # Update Cell State
+    puts "X: #{@x}, Y: #{@y}, State: #{@state}, #{[check.call(:trees), check.call(:lumber)]}"
     @state = case @state
       when :lumber
-        if check.call(:lumber) && check.call(:trees) \
-          then :lumber else :open end
+        unless check.call(:lumber).zero? or
+            check.call(:trees).zero? then
+          :lumber else :open end
       when :trees
         if check.call(:lumber) >= 3 then
           :lumber else :trees end
@@ -29,6 +31,14 @@ class Tile
         if check.call(:trees) >= 3 then
           :trees else :open end
     end; self
+  end
+  # Select Color
+  def color
+    Color.coerce case @state
+      when :lumber; "#FF4136"
+      when :trees; "#0074D9"
+      when :open; "#111111"
+    end
   end
   # Prettier Print
   def inspect
@@ -38,23 +48,35 @@ end
 class World < Hash
   # Parse File into World
   def initialize(fname)
-    y, x = 1, 0
+    @y, @x = 1, 0
     File.open(fname) do |file|
-      file.each_char do |chr|
-        if chr == "\n" then [y += 1, x = 0] else
-          self[y, x+=1] = Tile.new(y, x, chr)
+      file.read.chop.each_char do |chr|
+        if chr == "\n" then [@y += 1, @x = 0] else
+          self[@y, @x+=1] = Tile.new(@y, @x, chr)
         end
       end
     end
   end
   # Step Forwards
   def step(n = 10)
-    n.times do |i| merge Hash[map \
+    @image = Image.new(@x, (n+1) * (@y + 1))
+    render; n.times do |i| merge Hash[self.map\
       {|k, value| [k, value.update(self)]}]
-    end; self
+    render(i+1) end
+    @image.save("debug.bmp", :bmp)
+    return self
   end
   # Count & Multiply
   def resolve
+    select{|_, t| t.state == :lumber}.count \
+    * select{|_, t| t.state == :trees}.count
+  end
+  # Display Map
+  def render(z = 0)
+    @y.times do |y| @x.times do |x|
+      @image[x, z + (z * @y) + y] = self[y+1, x+1].color
+    end end; @image[0..@x-1, z + (z + 1) * @y] \
+      = Image.new @x, 1, (Color.coerce "#01FF70")
   end
   # Fuck Nested Arrays
   def []=(y, x, v)
@@ -66,4 +88,5 @@ class World < Hash
 end
 # Run Example
 example = World.new('test.txt')
-example.step
+example.step 2
+puts example.resolve
