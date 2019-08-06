9c69e607b165c7bf90c26a0e0cd04d4384e320f9
Sheky Sheks <sheks@desu.systems>
2019-08-06 12:59:50 -0400

    decompile

--- 2018/Day 21/gross.py
+++ 2018/Day 21/gross.py
@@ -1,41 +1,58 @@
-import operator, functools
-with open('input.txt') as f:
+import operator, functools, collections
+with open('2018/Day 21/input.txt') as f:
     ip, *tape = f.readlines()
-def compile(xs):
+r, codes, ip = "ABCDEF", {
+    'add': '+',
+    'mul': '*',
+    'ban': '&',
+    'bor': '|',
+}, int(ip.split()[1])
+# Day 21 Disassembler
+def right(g, a, b, c):
+    if c == b:
+        return f"{r[c]} {g}= {r[a]}"
+    if c == a:
+        return f"{r[c]} {g}= {r[b]}"
+    return f"{r[c]} = {r[a]} {g} {r[b]}"
+def left(g, a, b, c):
+    return f"{r[c]} {g}= {b}" if c == a \
+        else f"{r[c]} = {r[a]} {g} {b}"
+def assign(k, a, b, c):
+    return f"{r[c]} = {r[a] if k == 'r' else a}"
+def compare(g, kk, a, b, c):
+    if kk == 'ir':
+        return f"{r[c]} = {g}({a}, {r[b]})"
+    if kk == 'ri':
+        return f"{r[c]} = {g}({r[a]}, {b})"
+    if kk == 'rr':
+        return f"{r[c]} = {g}({r[a]}, {r[b]})"
+def disassemble(xs):
     inst, *ys = xs.split()
-    return (*ops[inst], *map(int, ys))
-def execute(xs, op, r1, r2, i, v, l):
-    xs[l] = op(xs[i] if r1 else i, xs[v]
-        if r2 else v); return xs
-ip, ops = int(ip.split()[1]), {
-    'addr': (operator.add, True, True), # addr
-    'addi': (operator.add, True, False), # addi
-    'mulr': (operator.mul, True, True), # mulr
-    'muli': (operator.mul, True, False), # muli
-    'banr': (operator.and_, True, True), #banr
-    'bani': (operator.and_, True, False), # bani
-    'borr': (operator.or_, True, True), #borr
-    'bori': (operator.or_, True, False), # bori
-    'setr': (lambda a,b: a, True, None), # setr
-    'seti': (lambda a,b: a, False, None), # seti
-    'gtir': (operator.gt, False, True), # gtir
-    'gtri': (operator.gt, True, False), # gtri
-    'gtrr': (operator.gt, True, True), # gtrr
-    'eqir': (operator.eq, False, True), # eqir
-    'eqri': (operator.eq, True, False), # eqri
-    'eqrr': (operator.eq, True, True), # eqrr
-}; tape = [*map(compile, tape)]
-def factors(n):
-    return set(functools.reduce(list.__add__, ([i, n//i]
-        for i in range(1, int(n**0.5)+1) if not n%i)))
-def scan(i = 0):
-    state = [i]+[0]*5
-    while state[ip] < len(tape):
-        inst = tape[state[ip]]
-        if inst[0] == ops['eqrr'][0]:
-            a,b,c = inst[-3:]
-            return state[b if a is c else a]
-        execute(state, *inst)
-        state[ip] += 1
-print('Silver:', sum(factors(scan(0))))
-print('Gold:', sum(factors(scan(1))))
+    ys = map(int, ys)
+    if inst[:-1] in codes:
+        code = codes[inst[:-1]]
+        if inst[-1] == 'r':
+            return right(code, *ys)
+        else:
+            return left(code, *ys)
+    if 'set' in inst:
+        return assign(inst[-1], *ys)
+    if 'gt' in inst:
+        return compare('GT', inst[-2:], *ys)
+    if 'eq' in inst:
+        return compare('EQ', inst[-2:], *ys)
+# Day 21 Decompiler
+lines = [*map(disassemble, tape)]
+updated = collections.defaultdict(str)
+for i, ln in enumerate(lines):
+    if f"{r[ip]} += 1" in ln:
+        updated[i-1] = updated[i-1].replace('not ', '')
+    elif f"{r[ip]} +=" in ln:
+        updated[i] = f"  if not {lines[i-1][2]} do"
+    elif f"{r[ip]} =" in ln:
+        jmp = int(ln.split('= ')[-1])+1; updated[i] = f"{4*' '}goto label-{jmp}"
+        updated[jmp] = f'label-{jmp}: {"{"+r[ip]}: {jmp-1}{"}"}\n' + updated[jmp]
+    else:
+        updated[i] += '  ' + ln
+for k in sorted(updated):
+    print(updated[k])
