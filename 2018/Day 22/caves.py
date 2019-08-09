03acaa928984fee018143451a2eff3d16e820239
Sheky Sheks <sheks@desu.systems>
2019-08-08 20:55:57 -0400

    python cleaner

--- 2018/Day 22/caves.py
+++ 2018/Day 22/caves.py
@@ -1,21 +1,19 @@
-x0, y0 = 8,713
-depth = 3879
-# Logic
-states, yc, xc = {
-    (x0, y0): depth % 20183,
-    (0,0): depth % 20183,
-}, 16807, 48271
-def calculate(x, y):
-    if states.get((x,y)):
-        return
-    if not y:
-        temp = x * yc
+# Global State
+states = dict()
+# Functions
+def erode(x, y):
+    if not (x,y) or (x,y) == target:
+        value = 0
+    elif not y:
+        value = x * 16807
     elif not x:
-        temp = y * xc
+        value = y * 48271
     else:
-        temp = states[x-1, y] * states[x, y-1]
-    states[x, y] = (temp + depth) % 20183
-for y in range(y0+1):
-    for x in range(x0+1):
-        calculate(x, y)
-sum(s%3 for s in states.values())
+        value = states[x-1, y] * states[x, y-1]
+    states[x, y] = (value + depth) % 20183
+    return states[x, y]
+# Run Problem
+depth, target = 3879, (8, 713)
+silver = sum(erode(x,y) % 3 for x in
+    range(target[0]+1) for y in range(target[1]+1))
+print("Silver:", silver)
