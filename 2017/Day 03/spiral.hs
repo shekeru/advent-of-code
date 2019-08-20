364e917bbf04a8e659f01f7599c3d12668e47540
Sheky Sheks <sheks@desu.systems>
2019-08-20 16:43:16 -0400

    finally

--- 2017/Day 03/spiral.hs
+++ 2017/Day 03/spiral.hs
@@ -1,14 +1,22 @@
 {-# LANGUAGE PartialTypeSignatures #-}
 module Main where
 
+import Data.Function
+import Control.Monad
 import Text.Printf
+import Data.Map
+
+type Spiral = Map Coords Int
+type Coords = (Int, Int)
 
 main :: IO()
 main = do
     let input = 361527
     printf "Silver: %d\n" $ part1 input
-    -- printf "Gold: %d\n" $ head (filter
-    --     (>input) $ concat grid)
+    printf "Gold: %d\n" $ part2 input
+
+part2 :: Int -> Int
+part2 x = head $ dropWhile (< x) spiral
 
 part1 :: Int -> Int
 part1 x = layer + abs offset where
@@ -16,8 +24,21 @@ part1 x = layer + abs offset where
   offset = layer - mod (x - start) (2 * layer)
   start = (2 * layer -1) ^ 2
 
-points :: [(Int, Int)]
-points = zip (shell 1 1 0) (shell 1 0 0)
+spiral :: [Int]
+spiral = maximum.elems <$> scanl reduce
+  (singleton (0,0) 1) (nby <$> points)
+
+reduce :: Spiral -> [Coords] -> Spiral
+reduce z xs = insert (xs !! 4) (sum $ ins <$> xs) z
+  where ins k = findWithDefault 0 k z
+
+nby :: Coords -> [Coords]
+nby = uncurry $ on (liftM2 (,)) go
+  where go n = [n-1..n+1]
+
+points :: [Coords]
+points = tail $ zip
+  (shell 1 1 0) (shell 0 1 0)
 
 shell :: Int -> Int -> Int -> [Int]
 shell tick dest prev = replicate tick prev ++ [prev,prev+y..dest-y]
--- README.md
+++ README.md
@@ -4,6 +4,6 @@ Status: Utterly Brainlet Filtered :joy_cat:
 Advent of Code is an Advent calendar of small programming puzzles for a variety of skill sets and skill levels that can be solved in any programming language you like. People use them as a speed contest, interview prep, company training, university coursework, practice problems, or to challenge each other.
 ## Calendar Progress
 - 2018: 21/25
-- 2017: 02/25
+- 2017: 06/25
 - 2016: 03/25
 - 2015: 08/25
