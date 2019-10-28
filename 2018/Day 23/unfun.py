76bff3fc1f029d283d693786afc9aa4ff9baa223
Sheky Sheks <sheks@desu.systems>
2019-10-28 10:06:55 -0400

    cleaner

--- 2018/Day 23/unfun.py
+++ 2018/Day 23/unfun.py
@@ -1,4 +1,4 @@
-import re
+import re, math
 with open('2018/Day 23/input.txt') as f:
     parse = lambda x: [*map(int, re.findall(r'-?\d+', x))]
     sys = [*map(parse, f.read().splitlines())]
@@ -6,20 +6,19 @@ st = max(sys, key = lambda pt: pt[3])
 def m_dist(xs, ys):
     solve = lambda x, y: abs(y - x)
     return sum(solve(*pts) for pts in zip(xs[:3], ys[:3]))
+amnt = [pts for pts in sys if m_dist(st, pts) <= st[3]]
 # Fucking Part 2 Garbage
-def flatten(sys):
-    for ys in sys:
-        dist = m_dist((0, 0, 0), ys)
-        if dist > ys[3]:
-            yield (dist - ys[3], True)
-            yield (dist + ys[3], False)
 def solved():
-    for value, ok in sorted(flatten(sys)):
-        if not ok:
-            break
-        yield value
+    values, lowest = set(), math.inf
+    for ys in sys:
+        dist = m_dist(3*[0], ys)
+        if dist > ys[3]:
+            lower = dist + ys[3]
+            values.add(dist - ys[3])
+            if lower < lowest:
+                lowest = lower
+    return max(x for x in values if x < lowest)
 # Printing
-print("Silver:", len([pts for pts in sys if
-    m_dist(st, pts) <= st[3]]))
-print("Gold:", 
-    [*solved()][-1])
+print("Silver:", len(amnt))
+print("Gold:", solved())
+
