2aab97428ec71ee8d32c18c847ec8564d300a043
Sheky Sheks <sheks@desu.systems>
2019-10-28 09:37:55 -0400

    still shouldnt work

--- 2018/Day 23/unfun.py
+++ 2018/Day 23/unfun.py
@@ -1,24 +1,25 @@
-import re, queue
+import re
 with open('2018/Day 23/input.txt') as f:
     parse = lambda x: [*map(int, re.findall(r'-?\d+', x))]
     sys = [*map(parse, f.read().splitlines())]
+st = max(sys, key = lambda pt: pt[3])
 def m_dist(xs, ys):
     solve = lambda x, y: abs(y - x)
     return sum(solve(*pts) for pts in zip(xs[:3], ys[:3]))
-st = max(sys, key = lambda pt: pt[3])
+# Fucking Part 2 Garbage
+def flatten(sys):
+    for ys in sys:
+        dist = m_dist((0, 0, 0), ys)
+        if dist > ys[3]:
+            yield (dist - ys[3], True)
+            yield (dist + ys[3], False)
+def solved():
+    for value, ok in sorted(flatten(sys)):
+        if not ok:
+            break
+        yield value
+# Printing
 print("Silver:", len([pts for pts in sys if
     m_dist(st, pts) <= st[3]]))
-# Fucking Part 2 Garbage
-pq = queue.PriorityQueue()
-for ys in sys:
-    dist = m_dist((0, 0, 0), ys)
-    pq.put((max(0, dist - ys[3]), 1))
-    pq.put((1 + dist + ys[3], -1))
-result, n, last = 0, 0, 0
-while not pq.empty():
-    dist, delta = pq.get()
-    n += delta
-    if n > last:
-        last = n
-        result = dist
-print("Gold:", result)
+print("Gold:", 
+    [*solved()][-1])
