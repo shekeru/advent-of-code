87535ee37f34274a73f9c00273ae2013f1ad5e2f
Sheky Sheks <sheks@desu.systems>
2019-12-05 04:33:32 -0500

    cleaner

--- 2019/Day 05/long.rb
+++ 2019/Day 05/long.rb
@@ -8,16 +8,12 @@ class Machine
     op = @xvs[@idx].to_s.rjust(4, '0').chars
     op_code, @im = op.pop(2).join.to_i,
       op.reverse.map{|x| x == '1'}
-    p1, p2, p3 = @params = @xvs[@idx+1, 3]
-    case op_code
-    when 99
-      return @cout
+    @params = @xvs[@idx+1, 3]
+      case op_code
     when 8
-      @xvs[p3] = pts(1) ==
-        pts(2) ? 1 : 0; @idx += 4
+      inst(3) {pts(1) == pts(2) ? 1:0}
     when 7
-      @xvs[p3] = pts(1) <
-        pts(2) ? 1 : 0; @idx += 4
+      inst(3) {pts(1) < pts(2) ? 1:0}
     when 6
       @idx = pts(1).zero? ?
         pts(2) : @idx + 3
@@ -27,18 +23,21 @@ class Machine
     when 4
       @cout << pts(1); @idx += 2
     when 3
-      @xvs[p1] = @cin.shift; @idx += 2
+      inst(1) {@cin.shift}
     when 2
-      @xvs[p3] = pts(1) * pts(2); @idx += 4
+      inst(3) {pts(1) * pts(2)}
     when 1
-      @xvs[p3] = pts(1) + pts(2); @idx += 4
-    end; false
-  end
+      inst(3) {pts(1) + pts(2)}
+    else return
+  end end
   def program(*cin)
     @cin, @cout = cin, []
-    loop do value = execute
-      return value.last if value
-    end
+    while value = execute
+      end; @cout.last
+  end
+  def inst(i)
+    @xvs[@params[i - 1]] =
+      yield; @idx += i + 1
   end
   def pts(i)
     v = @params[i -= 1]
