34f64803fa4100b4d57a1c489fae050bdce59084
Sheky Sheks <sheks@desu.systems>
2019-12-06 18:52:34 -0500

    you

--- 2019/Day 06/map.hs
+++ 2019/Day 06/map.hs
@@ -1,29 +1,27 @@
+import Data.List ((\\))
 import Data.Function (on)
 import Text.Printf (printf)
 import Data.List.Split (splitOn)
-import Data.Map.Strict hiding (foldl,
-  fromList, (\\), union)
-import Data.Set (fromList, (\\), union)
-import Prelude hiding (lookup, insert)
-type Graph = Map String String
+import qualified Data.Map.Strict as SM
+type Graph = SM.Map String String
 
 main :: IO()
 main = do
-  sys <- (\s -> mapWithKey (\k _
+  sys <- (\s -> SM.mapWithKey (\k _
     -> flatten s k) s) <$> input
   printf "Silver: %d\n" $foldl
     (\s x -> s + length x) 0 sys
   printf "Gold: %d\n" $length $on
-    (\x y -> union (x\\y) $y\\x)
-    (fromList.(!)sys) "SAN" "YOU"
+    (\x y -> (x\\y) <> (y\\x))
+    (sys SM.!) "SAN" "YOU"
 
 flatten :: Graph -> String -> [String]
-flatten xvs k = case lookup k xvs of
+flatten xvs k = case SM.lookup k xvs of
   Just v -> v : flatten xvs v
   Nothing -> mempty
 
 input :: IO Graph
-input = foldl fn empty.lines
+input = foldl fn SM.empty.lines
   <$> readFile "input.txt" where
-  fn xvs ln = insert ch pr xvs
+  fn xvs ln = SM.insert ch pr xvs
     where [pr, ch] = splitOn ")" ln
