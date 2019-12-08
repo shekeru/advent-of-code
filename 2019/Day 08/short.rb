e3cf2641f1c8fbc55a2d2ae34633f3ae897ac517
Sheky Sheks <sheks@desu.systems>
2019-12-08 07:49:37 -0500

    ruby d8

---
+++ 2019/Day 08/short.rb
@@ -0,0 +1,13 @@
+layers = File.read('ins.txt').strip
+  .chars.each_slice(6*25).to_a
+# Part 1 Min
+mins = layers.min_by{|xs|
+  xs.count{|x| '0' == x}}
+# Part 2 Text
+pixels = layers.reverse.transpose.map{
+  |xs| xs.inject{|a, e| '2' == e ? a : e}
+}.map{|x| '1' == x ? '■':' '}.
+  each_slice(25).map(&:join)
+# Display Everything
+puts "Silver: #{"12".chars.map{|c| mins
+  .count{|x| c == x}}.inject(:*)}", pixels
