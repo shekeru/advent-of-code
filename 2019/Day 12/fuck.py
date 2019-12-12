d4effff9a688d59eefab8ca010b0efe9272e39db
Sheky Sheks <sheks@desu.systems>
2019-12-12 16:29:57 -0500

    ah, fuck this

--- 2019/Day 12/fuck.py
+++ 2019/Day 12/fuck.py
@@ -1,5 +1,5 @@
-import numpy as np
-import itertools, re
+from itertools import combinations
+import numpy as np; import math, re
 # Shit Hawk Moons
 class Moon:
     def __init__(st, ln):
@@ -8,30 +8,40 @@ class Moon:
             map(np.array, ([*xs], [0] * 3))
     def __sub__(st, pt):
         return st.position - pt.position
-    def __repr__(st):
-        return f"{st.position} => {st.velocity}"
-    @property
     def total(st):
         fn = lambda xs: sum(map(abs, xs))
         return fn(st.position) * fn(st.velocity)
+    def hash(st):
+        return (*map(hash, zip(st.position, st.velocity)),)
 # Jupiter System
 class System:
     def __init__(s, fn):
         s.list = [*map(Moon, open('2019/Day 12/' +fn))]
-        s.pairs = [*itertools.combinations(s.list, 2)]
+        s.pairs, s.counter = [*combinations(s.list, 2)], 0
+        s.zeroes, s.ticks = (*s.values(),), [0] * 3
     def Update(s):
-        fn = lambda v: max(min(v, 1), -1)
+        s.counter += 1; fn = \
+    lambda v: max(min(v, 1), -1)
         for st, pt in s.pairs:
             offset = [*map(fn, pt - st)]
             st.velocity += offset
             pt.velocity -= offset
         for st in s.list:
             st.position += st.velocity
-            yield st.total
-    def Settle(s, n = 1000):
-        for x in range(n):
-            v = sum(s.Update())
-        return v
+            yield st.total()
+        for i, v in enumerate(s.values()):
+            if v == s.zeroes[i] and not s.ticks[i]:
+                s.ticks[i] = s.counter
+    def Solve(s, N = 1000):
+        while True:
+            value = sum(s.Update())
+            if s.counter == N: yield value;
+            if s.counter >= N and all(s.ticks):
+                a, b, c = s.ticks; yield a * b * c \
+            // math.gcd(a, b) // math.gcd(b, c)
+    def values(s):
+        return map(hash, zip(*(st.hash() for st in s.list)))
 # Fuck you Eric Wastl
-jupiter = System('ins.txt')
-print("Silver:", jupiter.Settle())
+local = System('ins.txt').Solve()
+print("Silver:", next(local))
+print("Gold:", next(local))
