0b3bf816854ff13f14ea1c12eaa4393ffceba3f1
Sheky Sheks <sheks@desu.systems>
2019-12-17 01:47:48 -0500

    fuck me

--- 2019/Day 16/fft.py
+++ 2019/Day 16/fft.py
@@ -1,21 +1,28 @@
 from itertools import *
 # Functions
+def to_int(xs):
+    return int("".join(map(str, xs)))
+base = (0, 1, 0, -1)
 def coeff(N, Idx):
     return base[(Idx+1) // (N+1) % 4]
 def nth(arr, Y = 100):
-    for _ in range(Y):
+    for i in range(Y):
         arr = [*fft(arr)]
-    return "".join(map(str, arr))
+    return to_int(arr[:8])
 def fft(arr):
     for N in range(len(arr)):
         digit = sum(x * coeff(N, i) for i,x in enumerate(arr))
         yield abs(digit) % 10
+# works because offset > n/2
+def n2_fft(xs, Y = 100):
+    v = (xs*10000)[to_int(xs[:7]):]
+    for _ in range(Y):
+        for x in range(-2, -len(v)-1, -1):
+            v[x] = (v[x]+v[x+1]) % 10
+    return to_int(v[:8])
 # Read Input
 with open('2019/Day 16/ins.txt') as f:
     XVS = [*map(int, f.read().strip())]
 # Display
-print(nth(XVS)[:8])
-#print(nth(XVS, 10000)[:8])
-
-base = (0, 1, 0, -1)
-arr = [1,2,3,4,5,6,7,8]
+print("Silver:", nth(XVS))
+print("Gold:", n2_fft(XVS))
