a93819a5fd24f464dbe4caa6d83e57e863f2846a
Sheky Sheks <sheks@desu.systems>
2020-04-20 07:39:29 -0400

    slightly cleaner

--- 2019/Day 18/class.py
+++ 2019/Day 18/class.py
@@ -1,18 +1,19 @@
 from heapq import *; import math
 from collections import *
-# Parsing Input
-Map, Keys = {}, {}; where = 'ins'
-with open(f'{where}.txt') as F:
+# Relative Positions
+def Nearby(x, y):
+    for j in (1, -1):
+        yield x + j, y
+        yield x, y + j
+# Load Maze File
+Map, Keys = {}, {}
+with open('ins.txt') as F:
     for y, Ln in enumerate(F.read().splitlines()):
         for x, Ch in enumerate(Ln):
             if Ch != '#':
                 if Ch.islower() or Ch == '@':
                     Keys[Ch] = y, x
                 Map[y, x] = Ch
-def Nearby(x, y):
-    for j in (1, -1):
-        yield x + j, y
-        yield x, y + j
 # World Class
 class World:
     def Move(s, To, Add = 0):
@@ -30,12 +31,12 @@ class World:
     def __lt__(s, obj):
         return s.Steps < obj.Steps
     def GenerateBFS(s):
-        Queue = deque([(0, *(Visited := {Keys[s.Where]}), set())])
+        Queue = deque([(0, *(Seen := {Keys[s.Where]}), set())])
         while Queue:
             Ln, Tile, _Doors = Queue.popleft()
             for St in Nearby(*Tile):
-                if all([St not in Visited, St in Map]):
-                    Doors = _Doors.copy(); Visited.add(St)
+                if all([St not in Seen, St in Map]):
+                    Doors = _Doors.copy(); Seen.add(St)
                     if (Ch := Map[St]).isupper():
                         Doors.add(Ch.lower())
                     if Ch.islower():
@@ -47,12 +48,12 @@ class World:
             s.GenerateBFS()
         for Ln, Ch, DrA in World.Cache[s.Where]:
             if not(Ch in s.Held or DrA - {*s.Held}):
-                yield (Ch, Ln)
+                yield s.Move(Ch, Ln)
 # This isn't even a graph anymore
 Queue = list(Seen := {World(): 0})
 while Queue:
     if (Opt := heappop(Queue)).Result:
-        print('Silver:', Opt); break
-    for Args in Opt.SearchCached():
-        if (Sp := Opt.Move(*Args)).Steps < Seen.get(Sp, math.inf):
+        print('Silver:', Opt.Steps); break
+    for Sp in Opt.SearchCached():
+        if Sp.Steps < Seen.get(Sp, math.inf):
             Seen[Sp] = Sp.Steps; heappush(Queue, Sp)
