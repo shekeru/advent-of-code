ac237f172985550ca0f6168413fd9b2714088cfd
Sheky Sheks <sheks@desu.systems>
2020-07-13 09:32:02 -0400

    another gold star

--- 2019/Day 17/main.hs
+++ 2019/Day 17/main.hs
@@ -1,32 +1,44 @@
 import Data.Vector (Vector, toList, (!), (//))
 import qualified Data.Set as S
+import Data.List
 import Text.Printf
 import Data.Char
 import IntCode
 
+data Face = North | East | South | West
 type Crds = (Int, Int)
 
 main :: IO()
 main = do
   tape <- input :: IO (Vector Int)
-  let retStr = map chr (fn tape)
-  let valid = intersects (lines retStr)
+  let tape' = tape // [(0, 2)]
+  let retStr = map chr (fn tape [])
+  let path = pathed (lines retStr)
+  let valid = intersects path
   printf "Silver: %d\n" $params valid
-  putStrLn retStr
+  printf "Gold: %d\n" $last $fn tape' solv
   where
-    fn xvs = eval (Program 0 0 xvs) []
+    fn xvs = eval (Program 0 0 xvs)
+    solv = map ord $ concat [
+      "C,A,A,C,B,A,B,B,A,C\n",
+      "R,8,L,6,L,6\n",
+      "L,10,R,10,L,6\n",
+      "R,10,R,8,L,10,L,10\n",
+      "n\n"]
 
 params :: [Crds] -> Int
 params = sum.map mul where
   mul (y, x) = y * x
 
-intersects :: [String] -> [Crds]
+pathed :: [String] -> [Crds]
+pathed xss = concatMap fix $
+  zip [0..] (map assign xss)
+
+intersects :: [Crds] -> [Crds]
 intersects xss = [pos |
     pos <- S.elems set, all
     (`S.member` set) (nby4 pos)]
-  where
-    idxd = zip [0..] (map assign xss)
-    set = S.fromList (concatMap fix idxd)
+  where set = S.fromList xss
 
 nby4 :: Crds -> [Crds]
 nby4 (y, x) = [(y+1, x), (y-1, x), (y, x-1), (y, x+1)]
