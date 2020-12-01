9abb9073b7213be743e02abd2470785f0c854d2e
Shekychan <sheks@desu.systems>
2020-12-01 16:42:30 -0500

    more variations

--- 2020/Day 01/2.exs
+++ 2020/Day 01/2.exs
@@ -1,21 +1,15 @@
 defmodule Day01 do
-  def nCr([], _), do: []
-  def nCr(_, 0), do: [[]]
-  def nCr([h|t], m) do
-    (for l <- nCr(t, m-1), do: [h|l])
-      ++ nCr(t, m)
-  end
-  def solve(what, nums, xt) do
+  def nCr([], _), do: []; def nCr(_, 0), do: [[]]
+  def nCr([h|t], m), do: (for l <- nCr(t, m-1), do: [h|l]) ++ nCr(t, m)
+  def solve(nums, {what, xt}) do
     v = Enum.find(nCr(nums, xt),
         fn xs -> 2020 == Enum.sum(xs) end)
       |> Enum.reduce(1, fn x, a -> x * a end)
-    "#{what}: #{v}"
-  end
+    IO.puts "#{what}: #{v}" end
   def main do
     nums = File.read!("input.txt")
       |> String.split(~r/\R/, trim: true)
       |> Enum.map(&String.to_integer/1)
-    solve("Silver", nums, 2) |> IO.puts
-    solve("Gold", nums, 3) |> IO.puts
-  end
+    [{"Silver", 2}, {"Gold", 3}] |>
+      Enum.map(&solve(nums, &1)) end
 end; Day01.main
