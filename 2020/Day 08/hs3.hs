8e793eefd8687cb28236353022d59aee4754c6e2
Shekychan <sheks@desu.systems>
2020-12-08 06:37:37 -0500

    [2020/08] compact negatives

--- 2020/Day 08/hs3.hs
+++ 2020/Day 08/hs3.hs
@@ -4,6 +4,7 @@ import qualified Data.Set as S
 import Control.Monad.State
 import Control.Lens
 import Text.Printf
+import Data.Either
 import Data.Maybe
 
 type Tape = V.Vector (String, Int)
@@ -12,23 +13,23 @@ type Active = (Int, Int, S.Set Int)
 main :: IO ()
 main = do
   tape <- input
-  printf "Silver: %d\n" (fst $eval tape `evalState`
-    start) >> printf "Gold: %d\n" (repair tape)
+  printf "Silver: %d\n" (fromLeft 0 $eval tape `evalState` start)
+  printf "Gold: %d\n" $repair tape
 start = (0, 0, S.empty)
 
 repair :: Tape -> Int
-repair xs = fst.fromJust.V.find snd $V.zip xs (V.enumFromN 0
-  $length xs) >>= pure.(`evalState` start).eval.uncurry fn where
-  fn ("jmp", v) = (xs&).(.~ ("nop", v)).ix; fn ("nop", v) =
-    (xs&).(.~ ("jmp", v)).ix; fn pair = const xs
+repair xs = fromRight 0.fromJust.V.find isRight $V.zip xs (V.enumFromN
+  0 $length xs) >>= pure.(`evalState` start).eval.uncurry fn where
+    fn ("jmp", v) = (xs&).(.~ ("nop", v)).ix; fn ("nop", v) =
+      (xs&).(.~ ("jmp", v)).ix; fn pair = const xs
 
-eval :: Tape -> State Active (Int, Bool)
+eval :: Tape -> State Active (Either Int Int)
 eval xs = do
   (acc, idx, seen) <- get; let (code, x) = xs V.! idx
-  if S.member idx seen then pure (acc, False) else do
+  if S.member idx seen then pure (Left acc) else do
     let acc' = (acc+) if code == "acc" then x else 0
     let idx' = (idx+) if code == "jmp" then x else 1
-    if idx >= V.length xs then pure (acc, True) else
+    if idx >= V.length xs then pure (Right acc) else
       put (acc', idx', S.insert idx seen) >> eval xs
 
 input :: IO Tape
