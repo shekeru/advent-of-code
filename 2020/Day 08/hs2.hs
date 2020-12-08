9876b4bcd5ac4879c08d9a6578b506d70d8933d8
Shekychan <sheks@desu.systems>
2020-12-08 06:37:37 -0500

    compact

--- 2020/Day 08/hs2.hs
+++ 2020/Day 08/hs2.hs
@@ -17,23 +17,19 @@ main = do
 start = (0, 0, S.empty)
 
 repair :: Tape -> Int
-repair xs = fst.fromJust.V.find snd $do
-  (i, x) <- V.zip (V.enumFromN 0 $length xs) xs
-  pure $evalState (eval $fn i x) start where
-    fn i ("jmp", v) = xs &ix i .~ ("nop", v)
-    fn i ("nop", v) = xs &ix i .~ ("jmp", v)
-    fn i x = xs
+repair xs = fst.fromJust.V.find snd $V.zip xs (V.enumFromN 0
+  $length xs) >>= pure.(`evalState` start).eval.uncurry fn where
+  fn ("jmp", v) = (xs&).(.~ ("nop", v)).ix; fn ("nop", v) =
+    (xs&).(.~ ("jmp", v)).ix; fn pair = const xs
 
 eval :: Tape -> State Active (Int, Bool)
 eval xs = do
-  (acc, idx, seen) <- get
+  (acc, idx, seen) <- get; let (code, x) = xs V.! idx
   if S.member idx seen then pure (acc, False) else do
-    let (code, x) = xs V.! idx
     let acc' = (acc+) if code == "acc" then x else 0
     let idx' = (idx+) if code == "jmp" then x else 1
-    let seen' = S.insert idx seen
-    put (acc', idx', seen') >> if idx >= V.length
-      xs then pure (acc, True) else eval xs
+    if idx >= V.length xs then pure (acc, True) else
+      put (acc', idx', S.insert idx seen) >> eval xs
 
 input :: IO Tape
 input = V.fromList.map (fn.words).lines <$> readFile "input.txt" where
