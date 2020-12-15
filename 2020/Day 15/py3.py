79c32a4c375936e96d943565e9acf5092174e8b5
Shekychan <sheks@desu.systems>
2020-12-15 10:37:24 -0500

    day 14 input

--- 2020/Day 15/py3.py
+++ 2020/Day 15/py3.py
@@ -1,11 +1,12 @@
-Card, Turn = 0, len(Graph := {V: K for K, V
-    in enumerate([1,2,16,19,18,0], 1)})
+Graph = [0] * 30000000
+for Turn, Card in enumerate([1,2,16,19,18,0], 1):
+    Graph[Card] = Turn
 # Fucking Hell
 def TurnCards(Nth):
     global Turn, Card
     while Nth > Turn:
         Graph[Card], Card = Turn, Turn - \
-            Graph.get(Card, Turn); Turn += 1
+            (Graph[Card] or Turn); Turn += 1
     return Card
 # Results
 print("Silver:", TurnCards(2020))
