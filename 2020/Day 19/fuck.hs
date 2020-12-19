d9e1b200704efc3a048ec50bbcd73374e65b2624
Shekychan <sheks@desu.systems>
2020-12-19 11:14:08 -0500

    better

--- 2020/Day 19/fuck.hs
+++ 2020/Day 19/fuck.hs
@@ -7,20 +7,20 @@ import Data.List.Split
 main :: IO ()
 main = do
   (silver, strings) <- readF "input.txt"
-  let generate rxs = foldr ((+).fromEnum.elem mempty.eval (mkTree rxs 0)) 0 strings
-  on (printf "Silver: %d\nGold: %d\n") generate silver $foldr (\(k, words -> v)
-    -> SM.insert k v) silver [(8, "42 | 42 8"), (11, "42 31 | 42 11 31")]
+  let generate rxs = foldr ((+).fromEnum.elem "".eval (mkTree rxs 0)) 0 strings
+  on (printf "Silver: %d\nGold: %d\n") generate silver $foldr (\(k, v) ->
+    SM.insert k $words v) silver [(8, "42 | 42 8"), (11, "42 31 | 42 11 31")]
 data Rule = Basic (String -> [String]) | Complex [[Rule]]
 
 eval :: Rule -> String -> [String]
-eval (Complex rss) xs = concatMap (foldl ((.eval)
-  .(>>=)) [xs]) rss; eval (Basic ch) xs = ch xs
+eval (Complex rss) xs = rss >>=
+  foldl ((.eval).(>>=)) [xs]
+eval (Basic ch) xs = ch xs
 
 mkTree :: Header -> Int -> Rule
-mkTree graph ((SM.!) graph -> xs)
-  | elem '"' (head xs) = Basic fn
-  | otherwise = Complex $map (map (mkTree graph.read)) $splitOn ["|"] xs
-    where fn (c:str) | c == (head$read$head xs) = [str]; fn _ = []
+mkTree graph ((SM.!) graph -> xs) | elem '"' (head xs) = Basic fn
+  | otherwise = Complex $map (map $mkTree graph.read) $splitOn ["|"] xs
+    where fn (c:str) | c == (head.read.head) xs = [str]; fn _ = []
 
 readF :: String -> IO (Header, [String]); type Header = SM.Map Int [String]
 readF wh = (\[hd, bd] -> (SM.fromList $map fn hd, bd)).splitOn [""].lines <$> readFile
