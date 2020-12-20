4021577e36710cf3d29f88185534fc9d8ced1589
Shekychan <sheks@desu.systems>
2020-12-19 19:34:28 -0500

    better hs

---
+++ 2020/Day 19/fuck2.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE ViewPatterns, BlockArguments #-}
+
+import Text.Printf; import Data.Function
+import qualified Data.Map.Strict as SM
+import Data.List.Split
+
+main :: IO ()
+main = do
+  (silver, strings) <- readF "input.txt"
+  let generate rxs = foldr ((+).fromEnum.elem "".(mkTree rxs 0)) 0 strings
+  on (printf "Silver: %d\nGold: %d\n") generate silver $foldr (\(k, v) ->
+    SM.insert k $words v) silver [(8, "42 | 42 8"), (11, "42 31 | 42 11 31")]
+
+mkTree :: Header -> Int -> Rule; type Rule = (String -> [String])
+mkTree graph ((SM.!) graph -> xs) | elem '"' (head xs) = basic | otherwise = \str ->
+  map (mkTree graph.read) <$> splitOn ["|"] xs >>= foldl (flip concatMap) [str]
+    where basic (c:str) | c == (head.read.head) xs = [str]; basic _ = []
+
+readF :: String -> IO (Header, [String]); type Header = SM.Map Int [String]
+readF wh = (\[hd, bd] -> (SM.fromList $map fn hd, bd)).splitOn [""].lines <$> readFile
+  wh where fn (splitOn ":" -> ln) = let [idx, rule] = ln in (read idx, words rule)
