bc1a40cbb9a5aadf245a96da79c86e0f635b9ef8
Shekychan <sheks@desu.systems>
2021-06-20 01:27:15 -0400

    faster

--- 2015/Day 24/v1.py
+++ 2015/Day 24/v1.py
@@ -1,11 +1,12 @@
 import itertools, operator, functools
-# Sheky Input
-Input = [*map(int, open('2015/Day 24/ins.txt'))]
 # AoC 2015 Makes Me Sad
-def Solve(Groups = 3):
+def Solve(Groups = 3, Start = 2):
     Ideal = sum(Input) // Groups
-    for N in range(1, len(Input) - 1):
+    while sum(Input[:Start]) < Ideal:
+        Start += 1
+    for N in range(Start, len(Input) - Groups):
         if List := [functools.reduce(operator.mul, x, 1) for x in itertools
             .combinations(Input, N) if sum(x) == Ideal]: return min(List)
 # Why is it so freaking simple
-print("Silver:", Solve(), "\nGold:", Solve(4))
+Input = sorted(map(int, open('2015/Day 24/ins.txt')), reverse
+    = True); print("Silver:", Solve(), "\nGold:", Solve(4))
