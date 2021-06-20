c625f566b8e4be6f7c3e9918b0742c24f2bce4a0
Shekychan <sheks@desu.systems>
2021-06-20 02:33:00 -0400

    uhhh

--- 2018/Day 23/unfun.py
+++ 2018/Day 23/unfun.py
@@ -11,11 +11,11 @@ def Manhattan(Xs, Ys = (0, 0, 0)):
 Limit, Values, Largest = math.inf, set(), \
     max(System, key = lambda Pt: Pt[3])
 for Pt in System:
-    if (Dist := Manhattan((0, 0, 0), Pt)) > Pt[3]:
-        Limit = min(Limit, Dist + Pt[3])
+    if (Dist := Manhattan(Pt)) > Pt[3]:
         Values.add(Dist - Pt[3])
+    Limit = min(Limit, Dist + Pt[3])
 # Print Results
 print("Silver:", sum(1 for Pt in System if
     Manhattan(Largest, Pt) <= Largest[3]))
 print("Gold:", max(Pt for Pt in
-    Values if Pt < Limit))
+    Values if Pt <= Limit))
