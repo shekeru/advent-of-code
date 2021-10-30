5de0a5a0227a10765f7bdbfb771fd3cf33f26260
Shekychan <sheks@desu.systems>
2021-10-30 12:54:08 -0400

    2020/01: Replace Bad Generator Function

--- 2020/Day 01/second.py
+++ 2020/Day 01/second.py
@@ -1,18 +1,29 @@
-def Combinations(A, M):
+# def Combinations(A, M):
+#     if not M:
+#         yield []
+#         return
+#     if not A:
+#         return []
+#     for L in Combinations(A[1:], M-1):
+#         yield A[:1] + L
+#     yield from Combinations(A[1:], M)
+
+def Tails(Array):
+    for x in range(1, len(Array)):
+        yield Array[x:]
+
+def C2(Array, M):
     if not M:
-        yield []
-        return
-    if not A:
-        return []
-    for L in Combinations(A[1:], M-1):
-        yield A[:1] + L
-    yield from Combinations(A[1:], M)
+        yield []; return
+    for (First, *Others) in Tails(Array):
+        for Ys in C2(Others, M - 1):
+            yield [First] + Ys
 
 def Product(A, V = 1):
     return Product(A[1:], A[0] * V) if A else V
 
 def Solve(Rn):
-    return Product({sum(Y): Y for Y in Combinations(Xs, Rn)}[2020])
+    return Product({sum(Y): Y for Y in C2(Xs, Rn)}[2020])
 
 Xs = [int(x) for x in open('input.txt')]
 print("Silver:", Solve(2))
